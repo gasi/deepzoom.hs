0f2102dd8ebb1d72f1ed6f778d3bdeddef0e5502
Daniel Gasienica <daniel@gasienica.ch>
2009-11-01 22:25:45 -0800

    Cleaned up code.

--- deepzoom.hs
+++ deepzoom.hs
@@ -10,14 +10,31 @@ data Rectangle = Rectangle (Int, Int, Int, Int)
 instance Show Rectangle
    where show (Rectangle (l, t, r, b)) = "(" ++ show l ++ ", " ++ show t ++ ", " ++ show (r - l) ++ ", " ++ show (b - t) ++ ")"
 
+right :: Rectangle -> Int
+right (Rectangle (_, _, width, _)) = width
+
+bottom :: Rectangle -> Int
+bottom (Rectangle (_, _, _, height)) = height
+
+left :: Rectangle -> Int
+left (Rectangle (x, _, _, _)) = x
+
+top :: Rectangle -> Int
+top (Rectangle (_, y, _, _)) = y
+
+
+-- Image pyramid
+levels :: Rectangle -> [Rectangle]
+levels (Rectangle (0, 0, 1, 1)) = [Rectangle (0, 0, 1, 1)]
+levels bounds = [bounds] ++ (levels (Rectangle (0, 0, newWidth, newHeight)))
+where newWidth = ceiling $ (fromIntegral width) / 2
+  newHeight = ceiling $ (fromIntegral height) / 2
+  width = right bounds
+  height = bottom bounds
+  
 tiles :: Rectangle -> Int -> Int -> [Rectangle]
 tiles bounds size overlap = flatten (map (\x -> rows x size overlap) cs)
     where cs = columns bounds size overlap
-
--- Helper
-flatten :: [[a]] -> [a]
-flatten [] = []
-flatten (y:ys) = y ++ (flatten ys)
 
 columns :: Rectangle -> Int -> Int -> [Rectangle]
 columns (Rectangle (left, _, right, _)) _ _
@@ -43,25 +60,18 @@ rows (Rectangle (left, top, right, bottom)) size overlap = [firstBounds] ++ (row
         firstBounds  = Rectangle (left, firstTop, right, firstBottom)
         secondBounds = Rectangle (left, secondTop, right, secondBottom)
 
-levels :: Rectangle -> [Rectangle]
-levels (Rectangle (0, 0, 1, 1)) = [Rectangle (0, 0, 1, 1)]
-levels bounds = [bounds] ++ (levels (Rectangle (0, 0, newWidth, newHeight)))
-    where newWidth = ceiling $ (fromIntegral width) / 2
-          newHeight = ceiling $ (fromIntegral height) / 2
-          width = getRight bounds
-          height = getBottom bounds
+-- Create Deep Zoom XML manifest
+descriptorXML :: Int -> Int -> Int -> Int -> String -> String
+descriptorXML width height tileSize tileOverlap tileFormat = 
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ++
+    "<Image Format=\"" ++ tileFormat ++ "\" Overlap=\"" ++ show tileOverlap ++ "\" TileSize=\"" ++ show tileSize ++ "\" xmlns=\"http://schemas.microsoft.com/deepzoom/2008\">" ++
+    "<Size Height=\"" ++ show height ++ "\" Width=\"" ++ show width ++ "\"/>" ++ 
+    "</Image>"
 
-getRight :: Rectangle -> Int
-getRight (Rectangle (_, _, width, _)) = width
-
-getBottom :: Rectangle -> Int
-getBottom (Rectangle (_, _, _, height)) = height
-
-getLeft :: Rectangle -> Int
-getLeft (Rectangle (x, _, _, _)) = x
-
-getTop :: Rectangle -> Int
-getTop (Rectangle (_, y, _, _)) = y
+-- List helper
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten (y:ys) = y ++ (flatten ys)
 
 -- Create path if does not exist
 createPath :: String -> IO ()
@@ -71,19 +81,11 @@ createPath path = do
         then createDirectory path
         else return ()
 
-descriptorXML :: Int -> Int -> Int -> Int -> String -> String
-descriptorXML width height tileSize tileOverlap tileFormat = 
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ++
-    "<Image Format=\"" ++ tileFormat ++ "\" Overlap=\"" ++ show tileOverlap ++ "\" TileSize=\"" ++ show tileSize ++ "\" xmlns=\"http://schemas.microsoft.com/deepzoom/2008\">" ++
-    "<Size Height=\"" ++ show height ++ "\" Width=\"" ++ show width ++ "\"/>" ++ 
-    "</Image>"
-
 main = do
     image <- loadJpegFile input
     (width, height) <- imageSize image
     createPath tilesPath
     writeFile descriptorFileName (descriptorXML width height tileSize tileOverlap tileFormat)
-    --(width, height) <- imageSize image
     --image <- (resizeImage 2 2 image)
     --saveJpegFile 95 "test_files/0/0_0.jpg" image --1 "out.jpg" image
     --putStrLn $ show $ (width, height)
