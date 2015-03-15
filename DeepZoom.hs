0f29285aff75b25e9aa237f490c1c37519a4a626
Daniel Gasienica <daniel@gasienica.ch>
2015-03-14 23:48:39 -0700

    Start `DeepZoom` module over with `savePyramid`

--- DeepZoom.hs
+++ DeepZoom.hs
@@ -1,93 +1,56 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module DeepZoom
-( Bounds(Bounds)
-, columns
-, descriptorXML
-, height
-, level
-, levelBounds
-, levels
-, pyramid
-, rows
-, tiles
-, width
+( descriptorXml
+, mkPyramid
+, Pyramid
+, pyramidHeight
+, pyramidTileFormat
+, pyramidTileOverlap
+, pyramidTileSize
+, pyramidWidth
+, savePyramid
+, TileFormat(..)
 ) where
 
-import Data.Aeson (ToJSON)
-import GHC.Generics (Generic)
+-- | The tile file format.
+data TileFormat = PNG | JPEG deriving (Eq)
 
+instance Show TileFormat where
+    show JPEG = "jpg"
+    show PNG = "png"
 
-data Bounds = Bounds {
-              left :: Int,
-              top :: Int,
-              right :: Int,
-              bottom :: Int
-            } deriving (Eq, Generic, Show)
+-- | The image pyramid.
+data Pyramid = Pyramid
+    { pyramidWidth :: Int -- ^ The width of the image pyramid.
+    , pyramidHeight :: Int -- ^ The height of the image pyramid.
+    , pyramidTileSize :: Int -- ^ The tile size of the image pyramid.
+    , pyramidTileOverlap :: Int -- ^ The tile overlap of the image pyramid.
+    , pyramidTileFormat :: TileFormat -- ^ The tile format of the image pyramid.
+    } deriving (Eq, Show)
 
-instance ToJSON Bounds
+mkPyramid :: Int -- ^ width
+          -> Int -- ^ height
+          -> Int -- ^ tile size
+          -> Int -- ^ tile overlap
+          -> TileFormat -- ^ tile format
+          -> Pyramid
+mkPyramid width height tileSize tileOverlap tileFormat =
+    Pyramid width height tileSize tileOverlap tileFormat
 
-width :: Bounds -> Int
-width r =  right r - left r
-
-height :: Bounds -> Int
-height r =  bottom r - top r
-
-
--- Image pyramid
-levels :: Bounds -> [Bounds]
-levels (Bounds 0 0 1 1) = [Bounds 0 0 1 1]
-levels bounds = bounds : levels (Bounds 0 0 w h)
-  where w = lowerLevel $ width bounds
-        h = lowerLevel $ height bounds
-        lowerLevel x = ceiling $ fromIntegral x / 2
-
-columns :: Bounds -> Int -> Int -> [Bounds]
-columns b _ _
-    | width b <= 0 = []
-columns (Bounds l t r b) size overlap = x : columns xs size overlap
-    where xl  = max 0 (l - overlap)
-          xr  = min r (l + size + overlap)
-          xsl = min r (l + size)
-          xsb = r
-          x   = Bounds xl t xr b
-          xs  = Bounds xsl t xsb b
-
-rows :: Bounds -> Int -> Int -> [Bounds]
-rows b _ _
-  | height b <= 0 = []
-rows (Bounds l t r b) size overlap = x : rows xs size overlap
-  where xl  = max 0 (t - overlap)
-        xb  = min b (t + size + overlap)
-        xst = min b (t + size)
-        xsb = b
-        x   = Bounds l xl r xb
-        xs  = Bounds l xst r xsb
-
-tiles :: Bounds -> Int -> Int -> [Bounds]
-tiles bounds size overlap = concatMap (\x -> rows x size overlap) cs
-    where cs = columns bounds size overlap
-
-pyramid :: Bounds -> Int -> Int -> [(Int, Bounds, [Bounds])]
-pyramid bounds tileSize tileOverlap = zip3 (reverse [0..maxLevel]) levelBounds tileBounds
-    where numLevels = length levelBounds
-          maxLevel = numLevels - 1
-          levelBounds = levels (Bounds 0 0 (right bounds) (bottom bounds))
-          tileBounds = map (\x -> tiles x tileSize tileOverlap) levelBounds
-
-level :: (Int, Bounds, [Bounds]) -> Int
-level (l, _, _) = l
-
-levelBounds :: (Int, Bounds, [Bounds]) -> Bounds
-levelBounds (_, b, _) = b
-
--- Deep Zoom XML manifest
-descriptorXML :: Int -> Int -> Int -> Int -> String -> String
-descriptorXML width height tileSize tileOverlap tileFormat =
+-- | Returns the descriptor XML of a 'Pyramid'.
+descriptorXml :: Pyramid -> String
+descriptorXml (Pyramid width height tileSize tileOverlap tileFormat) =
     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" ++
-    "<Image Format=\"" ++ tileFormat ++
-    "    \" Overlap=\"" ++ show tileOverlap ++
-    "    \" TileSize=\"" ++ show tileSize ++
-    "    \" xmlns=\"http://schemas.microsoft.com/deepzoom/2008\">\n" ++
-    "    <Size Height=\"" ++ show height ++ "\" Width=\"" ++ show width ++ "\"/>\n" ++
+    "<Image xmlns=\"http://schemas.microsoft.com/deepzoom/2008\"\n" ++
+    "       Format=\"" ++ show tileFormat ++ "\"\n" ++
+    "       Overlap=\"" ++ show tileOverlap ++ "\"\n" ++
+    "       TileSize=\"" ++ show tileSize ++ "\">\n" ++
+    "    <Size Height=\"" ++ show height ++
+           "\" Width=\"" ++ show width ++ "\"/>\n" ++
     "</Image>\n"
+
+savePyramid :: Pyramid
+            -> FilePath -- ^ Destination of `.dzi` file.
+            -> IO ()
+savePyramid pyramid destination = do
+    writeFile destination xml
+        where xml = descriptorXml pyramid
--- main.hs
+++ main.hs
@@ -5,13 +5,11 @@ import System.Environment
 
 main :: IO ()
 main = do
-    [source] <- getArgs
-    image <- loadJpegFile source
-    (w, h) <- imageSize image
-    let levelBounds = levels (Bounds 0 0 w h)
-    levelImages <- mapM (levelImage image) levelBounds
-    let ls = zip [1..] levelImages
-    mapM_ (\(i, img) -> saveJpegFile 95 (levelPath i) img) ls
+    [source, destination] <- getArgs
+    convert source destination 254 1
     where
-        levelImage image b = resizeImage (width b) (height b) image
-        levelPath i = show i ++ ".jpg"
+        convert source destination tileSize tileOverlap = do
+            image <- loadJpegFile source
+            (w, h) <- imageSize image
+            let pyramid = mkPyramid w h tileSize tileOverlap JPEG
+            savePyramid pyramid destination
